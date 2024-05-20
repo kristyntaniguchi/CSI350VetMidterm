f22912efb35f1f11534d57cd2f5b8a07ba4ad65d
Kristyn Taniguchi <90283966+kristyntaniguchi@users.noreply.github.com>
2024-05-19 22:49:36 -0700

    dropdown view component adjusted views

---
+++ CSI350VetMidterm/Components/VeterinarianDropdownMenuViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using CSI350VetMidterm.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSI350VetMidterm.Components
+{
+    //Inherit from ViewComponent and import using statement
+    public class VeterinarianDropdownMenuViewComponent : ViewComponent
+    {
+        //The dropdown should show all of the veterinarians so we need ApplicationDbContext
+        private readonly ApplicationDbContext _context;
+
+        //Constructor
+        public VeterinarianDropdownMenuViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }//End of Constructor
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Get the veterinarian info
+            var vet = await _context.Veterinarians.ToListAsync();
+            return View(vet);
+        }//End of InvokeAsync()
+
+
+
+    }//End of class
+}
---
+++ CSI350VetMidterm/ViewComponents/VeterinarianDynamicDropdownMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSI350VetMidterm.ViewComponents
+{
+    //Inherit from ViewComponent and add its using statement Microsoft.AspNetCore.Mvc;
+    public class VeterinarianDynamicDropdownMenuViewComponent : ViewComponent
+    {
+        //Synchronous
+        //public IViewComponentResult Invoke()
+        //{
+        //    return View();
+        //}
+
+        //Async
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }//End of Invoke()
+
+    }//End of class
+}
